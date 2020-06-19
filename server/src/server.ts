204e8f72f2262647f715dc826ad26c7069e05056
Ailsoncf <ailson_cf@yahoo.com.br>
2020-06-19 15:13:47 +1200

    Back-End

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import path from 'path'
+import cors from 'cors'
+import routes from './routes'
+
+const app = express()
+
+const PORT = process.env.PORT || 3333
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+
+app.listen(PORT,() => console.log(`rodando em ${PORT}`))
