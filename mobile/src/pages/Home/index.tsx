ea9da1c2b9021f663a6dd2320fdb264a977182fe
Ailsoncf <ailson_cf@yahoo.com.br>
2020-06-25 15:46:03 +1200

    Mobile

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
-import React, { useState, useEffect, ChangeEvent } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Feather as Icon } from '@expo/vector-icons'
-import { View, Image, StyleSheet, Text, ImageBackground, TextInput, KeyboardAvoidingView, Picker } from 'react-native'
+import { View, Image, Text, ImageBackground, Alert } from 'react-native'
 import { RectButton } from 'react-native-gesture-handler'
 import { useNavigation } from '@react-navigation/native'
 import axios from 'axios'
 import RNPickerSelect from 'react-native-picker-select'
+
+import {styles, PickerStyle} from './styles'
 
 
 interface IBGEUFResponse {
@@ -25,10 +27,13 @@ const Home = () => {
   const navigation = useNavigation()
 
   function handleNavigateToPoints() {
-    navigation.navigate('Points', {
-      selectedUF,
-      selectedCity
-    })
+    if (selectedUF !== '0' && selectedCity !== '0') 
+      navigation.navigate('Points', {
+        selectedUF,
+        selectedCity
+      })
+    else
+      Alert.alert('Oooops...', 'Você precisa selecionar uma UF e uma cidade.')
   }
 
   function handleSelectedUF(value: string) {
@@ -124,87 +129,5 @@ const Home = () => {
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 32,
-  },
-
-  main: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-
-  title: {
-    color: '#322153',
-    fontSize: 32,
-    fontFamily: 'Ubuntu_700Bold',
-    maxWidth: 260,
-    marginTop: 64,
-  },
-
-  description: {
-    color: '#6C6C80',
-    fontSize: 16,
-    marginTop: 16,
-    fontFamily: 'Roboto_400Regular',
-    maxWidth: 260,
-    lineHeight: 24,
-  },
-
-  footer: {},
-
-  select: {},
-
-  input: {
-    height: 60,
-    backgroundColor: '#FFF',
-    borderRadius: 10,
-    marginBottom: 8,
-    paddingBottom: 20,
-    paddingHorizontal: 24,
-    fontSize: 16,
-  },
-
-  button: {
-    backgroundColor: '#34CB79',
-    height: 60,
-    flexDirection: 'row',
-    borderRadius: 10,
-    overflow: 'hidden',
-    alignItems: 'center',
-    marginTop: 8,
-  },
-
-  buttonIcon: {
-    height: 60,
-    width: 60,
-    backgroundColor: 'rgba(0, 0, 0, 0.1)',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-
-  buttonText: {
-    flex: 1,
-    justifyContent: 'center',
-    textAlign: 'center',
-    color: '#FFF',
-    fontFamily: 'Roboto_500Medium',
-    fontSize: 16,
-  }
-})
-
-const PickerStyle = StyleSheet.create({
-  inputAndroid: {
-    height: 60,
-    backgroundColor: '#FFF',
-    borderRadius: 10,
-    marginBottom: 8,
-    paddingBottom: 20,
-    paddingHorizontal: 24,
-    fontSize: 16,
-  }
-})
 
 export default Home
